00619bc4452697914185eaf4cefb7ad0bdfe9871
Maxim Tarasyants <macartoor@gmail.com>
2020-06-01 15:46:31 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/maxta/selenium/MainApp.java
@@ -0,0 +1,7 @@
+package com.geekbrains.maxta.selenium;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/geekbrains/maxta/selenium/MainAppTests.java
@@ -0,0 +1,27 @@
+package com.geekbrains.maxta.selenium;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainAppTests {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void init() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void openPage() {
+        driver.get("https://www.automationpractice.com");
+    }
+
+    @AfterEach
+    public void shutdown() {
+        driver.quit();
+    }
+
+}
